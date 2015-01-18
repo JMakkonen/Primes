0a34c275e463c34a244c976a2836cdc11237fc8d
JMakkonen <jarno.makkonen@gmail.com>
2015-01-17 19:12:30 -0800

    Added lcm function
    Added lcm(num1, num2) function which returns the least (or lowest)
    common multiple of num1 and num2.

--- P1004.rb
+++ P1004.rb
@@ -33,3 +33,33 @@ else
 end
 end
 
+def lcm(num1, num2)
+primes = ReadPrimes()
+fac1 = Factor(num1, primes)
+fac2 = Factor(num2, primes)
+
+lcm_fac = [] # We will be storing factors into this array
+while fac1.length() !=0 do
+  pos2 = -1
+  for i in (0..fac2.length()-1)
+    if fac2[i] == fac1[0] then
+      pos2 = i
+    end
+  end
+  if pos2 != -1 then
+    fac2.delete_at(pos2)
+  end
+  lcm_fac.push(fac1[0])
+  fac1.delete_at(0)
+end
+while fac2.length() !=0 do
+  lcm_fac.push(fac2[0])
+  fac2.delete_at(0)
+end
+# Multiply all the factors together to get the LCM.
+lcm = 1
+for i in (0..lcm_fac.length()-1) do
+  lcm = lcm * lcm_fac[i]
+end
+return lcm
+end
